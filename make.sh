c3edd0a2bec2e39cc0dd5f5c6f6e22ae2f9461f3
Artem V. Ageev <artem.v.ageev@gmail.com>
2024-10-22 21:46:05 +0300

    small fix make.sh

--- make.sh
+++ make.sh
@@ -1,33 +1,16 @@
 #!/usr/bin/env bash
-# if you compile first time you must change variable "lazpath" and "lcl"
-# after it execute this script with parameter "all" at awgg dir
-# "./build.sh all" it build awgg
-#                                                 by Segator
-# You can execute this script with different parameters:
-# default - compiling AWGG only (using by default)
 
-function f_log
-{
-    declare -rAi TAG=(
-        [error]=31
-        [info]=32
-        [audit]=33
-    )
-    printf '%(%y-%m-%d_%T)T\x1b[%dm\t%s:\t%b\x1b[0m\n' -1 "${TAG[${1,,:?}]}" "${1^^}" "${2:?}" 1>&2
-    if [[ ${1} == 'error' ]]; then
-        return 1
-    fi
-}
+function priv_clippit
+(
+    cat <<EOF
+Usage: bash ${0} [OPTIONS]
+Options:
+    build   Build program
+EOF
+)
 
-function f_build
-{
-    lazbuild --add-package 'peazip-sources/dev/metadarkstyle/metadarkstyle.lpk'
-    lazbuild 'peazip-sources/dev/project_peach.lpi'
-    lazbuild 'peazip-sources/dev/project_pea.lpi'
-}
-
-function f_main
-{
+function priv_main
+(
     set -eo pipefail
     if !(which lazbuild); then
         source '/etc/os-release'
@@ -35,12 +18,28 @@ function f_main
             debian | ubuntu)
                 sudo apt-get update
                 sudo apt-get install -y lazarus
-            ;;
+                ;;
         esac
     fi
-    case ${1} in
-        build) f_build;;
-    esac
-}
+    if ((${#})); then
+        case ${1} in
+            build) pub_build ;;
+        esac
+    else
+        priv_clippit
+    fi
+)
 
-f_main "${@}"
+function pub_build
+(
+    if {
+        pushd 'peazip-sources/dev'
+    }; then
+        lazbuild --add-package 'metadarkstyle/metadarkstyle.lpk'
+        lazbuild 'project_peach.lpi'
+        lazbuild 'project_pea.lpi'
+        strip peach pea
+    fi
+)
+
+priv_main "${@}"
